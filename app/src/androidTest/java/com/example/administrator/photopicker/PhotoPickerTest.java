9ec50e127d860fe7bae4480d35a4e1e41b5acb17
walaa <walaa>
2016-08-04 08:46:12 +0200

    code clean up

--- app/src/androidTest/java/com/example/administrator/photopicker/PhotoPickerTest.java
+++ app/src/androidTest/java/com/example/administrator/photopicker/PhotoPickerTest.java
@@ -2,10 +2,17 @@ package com.example.administrator.photopicker;
 
 import android.test.AndroidTestCase;
 
+import org.junit.Test;
+
 /**
  * Created by Administrator on 8/4/2016.
  */
 public class PhotoPickerTest extends AndroidTestCase {
+    @Test
+    public void checkEditTextValue(){
 
-
+    }
 }
+
+
+
