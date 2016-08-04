15db0ab579fa280a9267aecbbf89316d42588fb1
walaa <walaa>
2016-08-04 08:08:17 +0200

    code clean up

---
+++ app/src/androidTest/java/com/example/administrator/photopicker/PhotoPickerTest.java
@@ -0,0 +1,11 @@
+package com.example.administrator.photopicker;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by Administrator on 8/4/2016.
+ */
+public class PhotoPickerTest extends AndroidTestCase {
+
+
+}
